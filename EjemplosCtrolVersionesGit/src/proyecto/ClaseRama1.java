fe2a1d98e2dedf4183673d21f8ff62f90d2d82b1
JoseAMorales <joseantonio.morales.arnas@alumnos.upm.es>
2026-03-18 11:24:42 +0100

    rama1: ClaseRama1 con dos mensajes

---
+++ EjemplosCtrolVersionesGit/src/proyecto/ClaseRama1.java
@@ -0,0 +1,11 @@
+package proyecto;
+
+public class ClaseRama1 {
+
+	public static void main(String[] args) {
+		System.out.println("Mensaje 1 desde rama1");
+		System.out.println("Mensaje 2 desde rama1");
+
+	}
+
+}
